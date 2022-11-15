15f2ef7a516c050ac73590fd821ce4ce47b67a82
Jacob <jakehub24@gmail.com>
2022-11-15 18:35:18 -0500

    A bit of progress on the animations

--- VV_Lab_Rat_Fall_2022_Unity_File/Assets/Scripts/WalkingContrller.cs
+++ VV_Lab_Rat_Fall_2022_Unity_File/Assets/Scripts/WalkingContrller.cs
@@ -9,21 +9,45 @@ public class WalkingContrller : MonoBehaviour
     {
         mAnimator = GetComponent<Animator>();
     }
+    public string[] userInput = new string[4]{"w", "a", "s", "d"};
+    private int countingVariable = 0;
+    private bool isWalking = false;
 
     void Update()
     {
         if (mAnimator != null)
         {
-            if (Input.GetKeyDown(KeyCode.W))
+            if (isWalking)
             {
-                mAnimator.SetBool("Walk", true);
-                //mAnimator.Play("Walk");
+                KeyInput("w");
             }
-            if (Input.GetKeyUp(KeyCode.W))
+
+            KeyInput("a");
+            KeyInput("s");
+            KeyInput("d");
+            /*
+            KeyInput(userInput[countingVariable]);
+            countingVariable++;
+            if (countingVariable == 4)
             {
-                //mAnimator.StopPlayback();
-                mAnimator.SetBool("Walk", false);
+                countingVariable = 0;
             }
+            */
+        }
+    }
+
+    public void KeyInput(string k)
+    {
+        if (Input.GetKey(k))
+        {
+            mAnimator.SetBool("Walk", true);
+            isWalking = true;
+            Debug.Log(userInput[countingVariable]);
+        }
+        else if (Input.GetKey(k) == false)
+        {
+            mAnimator.SetBool("Walk", false);
+            isWalking = false;
         }
     }
 }
